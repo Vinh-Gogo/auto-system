41264177c1edcde918921dd04954b62b2047b837
Quang-Vinh Le <104542729+Vinh-Gogo@users.noreply.github.com>
2025-07-31 13:13:09 +0700

    add image generator

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import requests
 import matplotlib.pyplot as plt
+# You can access the image with PIL.Image for example
+import io
+from PIL import Image
 
 API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev"
-# Set your HuggingFace token here if not set in environment
-HF_TOKEN = os.environ.get('HF_TOKEN')
+HF_TOKEN = os.environ.get('HF_TOKEN') # Set HuggingFace environment
+
+# check
 if HF_TOKEN is None:
     raise ValueError("Please set the HF_TOKEN environment variable or assign your token to HF_TOKEN.")
 
@@ -12,19 +16,18 @@ headers = {
     "Authorization": f"Bearer {HF_TOKEN}",
 }
 
+# Get API URL
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.content
 
-image_bytes = query({
-    "inputs": "Beautiful girl in Vietnamese, wearing white dress, standing on the beach, sunlight shining",
-})
+def get_image(request):
+    payload = { "inputs": request,}
+    img_bytes = query(payload)
+    image = Image.open(io.BytesIO(img_bytes))
+    return image
 
-# You can access the image with PIL.Image for example
-import io
-from PIL import Image
-image = Image.open(io.BytesIO(image_bytes))
-
+image = get_image("Beautiful girl in Vietnamese, wearing white dress, standing on the beach, sunlight shining")
 plt.imshow(image)
 plt.axis("off")
 plt.show()
